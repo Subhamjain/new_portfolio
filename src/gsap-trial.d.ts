8ced73779e1544a8790965d514b9dfb3ecfb139f
Subham Jain <subham@Subhams-MacBook-Air.local>
2026-03-09 15:05:37 +0530

    fix build error

---
+++ src/gsap-trial.d.ts
@@ -0,0 +1,15 @@
+declare module "gsap-trial/ScrollSmoother" {
+  const ScrollSmoother: any;
+  export default ScrollSmoother;
+}
+
+declare module "gsap-trial/SplitText" {
+  export class SplitText {
+    chars: HTMLElement[];
+    words: HTMLElement[];
+    lines: HTMLElement[];
+    constructor(target: string | string[] | Element | Element[], vars?: object);
+    split(vars?: object): SplitText;
+    revert(): void;
+  }
+}
